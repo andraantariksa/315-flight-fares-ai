157983ee897123866aa73343735290ad1dfba29e
Andra Antariksa <andra.antariksa@gmail.com>
2021-04-13 22:01:24 +0700

    Fix issue on json decode error due to the page has been changed

--- traveloka-scrap/index.js
+++ traveloka-scrap/index.js
@@ -5,29 +5,23 @@ const fs = require('fs');
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
-  const iatas = 'CGK,HLP'.split(','); //,HLP,SUB,DPS,JOG,YIA,NRT,HKG,SIN,KUL
+  const iatas = 'CGK,HLP,DPS'.split(','); //,HLP,SUB,DPS,JOG,YIA,NRT,HKG,SIN,KUL
   const resps = [];
   const flightDatas = {};
 
-  for (let dayOfMonth = 1; dayOfMonth <= 10; dayOfMonth++) {
+  for (let dayOfMonth = 1; dayOfMonth <= 1; dayOfMonth++) {
     for (const iataDepart of iatas) {
       for (const iataArriv of iatas) {
         if (iataDepart === iataArriv) continue;
-        page.on('response', response => {
-          if (response.url().startsWith('https://www.traveloka.com/api/v2/flight/search/oneway')) {
-              resps.push(new Promise(async (resolve, reject) => {
-                const respData = await response.json();
-                // TODO
-                // Get all the necessary data
-                console.log(respData.data.searchId);
-                resolve();
-              }))
-          }
-        });
-        await page.goto(`https://www.traveloka.com/en-id/flight/fullsearch?ap=${iataArriv}.${iataDepart}&dt=${dayOfMonth}-06-2021.NA&ps=1.0.0&sc=ECONOMY`, {
+        await page.goto(`https://www.traveloka.com/en-id/flight/fullsearch?ap=${iataArriv}.${iataDepart}&dt=0${dayOfMonth}-06-2021.NA&ps=1.0.0&sc=ECONOMY`, {
           waitUntil: 'networkidle2',
           timeout: 50000,
         });
+        const resp = await page.waitForResponse('https://www.traveloka.com/api/v2/flight/search/oneway');
+        const respData = await resp.json();
+        // TODO
+        // Get all the necessary data
+        console.log(respData.data.searchId);
       }
     }
   }
