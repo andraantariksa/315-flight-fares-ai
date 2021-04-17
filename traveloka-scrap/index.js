f4f9e18b63b4124e859c7df6e10b643b07ae0646
Andra Antariksa <andra.antariksa@gmail.com>
2021-04-17 15:46:42 +0700

    Update the scrapper

--- traveloka-scrap/index.js
+++ traveloka-scrap/index.js
@@ -7,67 +7,51 @@ const fs = require('fs');
   let airport = {};
   let airline = {};
   const flightDatas = {};
-  const output = {};
-  const iatas = 'CGK,HLP,DPS'.split(','); //,HLP,SUB,DPS,JOG,YIA,NRT,HKG,SIN,KUL
-  const resps = [];
+  const iatas = 'CGK,HLP,SUB,DPS,JOG,YIA,NRT,HKG,SIN,KUL,MAN,YQB,MUC,BNE,CTU'.split(','); //,HLP,SUB,DPS,JOG,YIA,NRT,HKG,SIN,KUL
 
-  for (let dayOfMonth = 1; dayOfMonth <= 1; dayOfMonth++) {
-    const searchData = {};
-
+  for (let dayOfMonth = 2; dayOfMonth <= 10; dayOfMonth++) {
+    const flightDepartData = {};
     for (const iataDepart of iatas) {
+      const flightArrivalData = {};
       for (const iataArriv of iatas) {
         if (iataDepart === iataArriv) continue;
-        await page.goto(`https://www.traveloka.com/en-id/flight/fullsearch?ap=${iataArriv}.${iataDepart}&dt=0${dayOfMonth}-06-2021.NA&ps=1.0.0&sc=ECONOMY`, {
-          waitUntil: 'networkidle2',
-          timeout: 50000,
-        });
-        const resp = await page.waitForResponse('https://www.traveloka.com/api/v2/flight/search/oneway');
+        while (true) {
+          try {
+            await page.goto(`https://www.traveloka.com/en-id/flight/fullsearch?ap=${iataArriv}.${iataDepart}&dt=0${dayOfMonth}-06-2021.NA&ps=1.0.0&sc=ECONOMY`, {
+              waitUntil: 'networkidle2',
+              timeout: 50000,
+            });
+            break;
+          } catch(e) {
+            console.log(e);
+          }
+        }
+        let resp;
+        try {
+          resp = await page.waitForResponse('https://www.traveloka.com/api/v2/flight/search/oneway', { timeout: 5000 });
+        } catch (e) {
+          continue;
+        }
         const respData = await resp.json();
         
         // Filter search results
-        const searchResults = respData.data.searchResults;
-        let filteredResults = [];
-        for (const result of searchResults) {
-          let finalResult = {};
-          finalResult.totalNumStop = result.totalNumStop;
-          finalResult.mAppsPrice = result.mAppsPrice;
-          
-          let filteredConnectingFlight = [];
-          const connectingFlightRoutes = result.connectingFlightRoutes
-          for (const connectingFlight of connectingFlightRoutes){
-            let finalConnectingFlight = {};
-            finalConnectingFlight.departureAirport = connectingFlight.departureAirport;
-            finalConnectingFlight.arrivalAirport = connectingFlight.arrivalAirport;
-            finalConnectingFlight.numDayOffset = connectingFlight.numDayOffset;
-            finalConnectingFlight.totalNumStop = connectingFlight.totalNumStop;
-            finalConnectingFlight.segments = connectingFlight.segments;
-            
-            filteredConnectingFlight.push(finalConnectingFlight);
-          }
-          
-          finalResult.connectingFlightRoutes = connectingFlightRoutes;
-          filteredResults.push(finalResult);
-        }
-        
-        searchData[iataDepart+'to'+iataArriv] = filteredResults;
         airline = Object.assign(airline, respData.data.airlineDataMap);
         airport = Object.assign(airport, respData.data.airportDataMap);
         
+        flightArrivalData[iataArriv] = respData.data.searchResults;
+
         console.log(respData.data.searchId);
       }
+      flightDepartData[iataDepart] = flightArrivalData;
     }
 
-    flightDatas[20210600+dayOfMonth] = searchData;
+    fs.writeFileSync(`output-${20210600+dayOfMonth}.json`, JSON.stringify({
+      airline,
+      airport,
+      flightDatas: flightDepartData
+    }));
+    // flightDatas[20210600+dayOfMonth] = flightDepartData;
   }
 
-  output.airline = airline;
-  output.airport = airport;
-  output.flightDatas = flightDatas;
-
-  await Promise.all(resps);
   await browser.close();
-
-
-
-  fs.writeFileSync('output.json', JSON.stringify(output));
 })();
